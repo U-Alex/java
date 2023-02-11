31a37e1124330b5152f3470a60da9ebbcf14d4c4
UAA <uaa04@yandex.ru>
2023-02-11 15:45:57 +0900

    task2

--- HW_1/Task_2.java
+++ HW_1/Task_2.java
@@ -1,7 +1,32 @@
 package HW_1;
 /*2. Вывести все простые числа от 1 до 1000 */
 
+import java.util.ArrayList;
 
 public class Task_2 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> simple_list = new ArrayList<Integer>();
+        for (int num = 2; num <= 1000; num++) {
+            if (isSimple(num)) simple_list.add(num);
+        }
+        printList(simple_list);
+    }
+    private static boolean isSimple(int num) {
+        boolean simple = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                simple = false;
+                break;
+            }
+        }
+        return simple;
+    }
+    private static void printList(ArrayList<Integer> sl) {
+        for (int i = 0; i < sl.size(); i++) {
+            if (i == 0) System.out.println("простые числа от 1 до 1000");
+            else if (i % 8 == 0) System.out.println();
+            System.out.printf(" %d\t", sl.get(i));
+        }
+        return;
+    }
 }
